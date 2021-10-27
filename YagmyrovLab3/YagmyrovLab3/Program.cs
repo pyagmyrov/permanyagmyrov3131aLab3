f7baf9889ed1ef1f4f2b1d3b8a2267a5a2819ede
Perman Yagmyrov <perman.yagmyrov@eloop.app>
2021-10-27 23:20:19 +0300

    update

---
+++ YagmyrovLab3/YagmyrovLab3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YagmyrovLab3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
